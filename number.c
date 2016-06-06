c61b2ee788251e45becad991b28b2c332c605dc3
3vinitha333 <vinisinnu333@gmail.com>
2016-06-06 23:27:29 +0530

    Update and rename .gitignore to number.c

---
+++ number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+{
+  int n;
+  scanf("%d",&n);
+  if(n>0)
+  {
+  	printf("positive number");
+  }
+else if(n<0)
+  {
+  	printf("negative number");
+  }
+  
+  
+  else
+  printf("zero");
+ 
+}
+   
